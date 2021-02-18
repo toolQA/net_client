2f0400d0699351d7e52060a3b2bb4d414aac166f
tkstk <549233474@qq.com>
2021-02-18 16:42:07 +0800

    ok

---
+++ Assets/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public UdpClient udpClient = null;
+
+    public GameObject btnGo_sendMsg = null;
+
+    public InputField input_msg = null;
+
+    public InputField input_ip = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input_ip.onEndEdit.AddListener(JudgeIsCorrectIPV4);
+
+        Tools.SetButton(btnGo_sendMsg, Btn_SendMsg);
+    }
+
+    private void JudgeIsCorrectIPV4(string arg0)
+    {
+        if (Tools.IsCorrectIPV4(arg0))
+        {
+            udpClient.Init(arg0, GetNetMsg);
+        }
+        else
+        {
+            Debug.LogError("【error】server ip is not correct.");
+        }
+    }
+
+    private void Btn_SendMsg()
+    {
+        udpClient.Send(input_msg.text);
+    }
+
+    private void GetNetMsg(string txt)
+    {
+        
+    }
+}
---
+++ Assets/UdpClient.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+
+public class UdpClient : MonoBehaviour
+{
+    ToolDelegate.String recvCB = null;
+    private Socket socket = null;
+    private EndPoint targetEP = null;
+    private string m_ip = "127.0.0.1";
+    private int m_port = 8080;
+    private bool isRunning = false;
+
+    public void Init(string targetIP, ToolDelegate.String _recvCB)
+    {
+        recvCB = _recvCB;
+
+        Quit();
+
+        if (!string.IsNullOrEmpty(m_ip))
+        {
+            m_ip = targetIP;
+        }
+
+        targetEP = (EndPoint)(new IPEndPoint(IPAddress.Parse(m_ip), m_port));
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Send("init");
+    }
+
+    public void Send(string info)
+    {
+        try
+        {
+            byte[] byData = System.Text.Encoding.UTF8.GetBytes(info);
+
+            socket.SendTo(byData, targetEP);
+
+            Debug.Log("发送 " + info);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("send " + e.Message);
+        }
+    }
+
+    public void Quit()
+    {
+        if (isRunning)
+        {
+            socket.Close();
+
+            Debug.LogWarning(m_ip + " server close");
+        }
+    }
+}
